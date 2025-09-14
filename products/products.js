410f1ccfe23090725a777b98251fa9d051480e17
Madhankumar-7923 <madhankumar13148@gmail.com>
2025-09-14 16:48:30 +0530

    Seventh Commit

--- products/products.js
+++ products/products.js
@@ -1,11 +1,13 @@
 /*SEARCH FUNCTION*/
 var productcontainer = document.querySelector(".content");
 var search = document.getElementById("search");
+var nullSearchText = document.getElementById("search_Dummy")
 var productlist = productcontainer.querySelectorAll(".content_box");
 
 search.addEventListener("keyup", function () {
 
     var enteredvalue = event.target.value.toUpperCase();
+    var found = false;
 
     for (i = 0; i < productlist.length; i++) {
         var productname = productlist[i].querySelector(".content_box_title").textContent;
@@ -14,10 +16,20 @@ search.addEventListener("keyup", function () {
             productlist[i].style.display = "none";
         }
 
-        else{
+        else {
             productlist[i].style.display = "flex";
             productcontainer.style.justifyContent = "space-evenly";
+            found = true;
         }
+    }
+
+    if (found) {
+        productcontainer.style.justifyContent = "space-evenly";
+        nullSearchText.style.display = "none"; // hide "no result"
+    } 
+    
+    else {
+        nullSearchText.style.display = "flex"; // show "no result"
     }
 
 });
